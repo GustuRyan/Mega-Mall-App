51bdc4d8209a0131ae4c06bbf4b0a09f21dad798
Gus Ryan <120316725+GustuRyan@users.noreply.github.com>
2025-04-07 23:02:48 +0800

    update today schedule widget

--- lib/widgets/today_schedule.dart
+++ lib/widgets/today_schedule.dart
@@ -1,3 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:hr_360/core/style/style.dart';
 import 'package:hr_360/core/utils/asset_util.dart';
+
+class HomeTodaySchedule extends StatelessWidget {
+  final Widget content;
+  final Widget time;
+}
