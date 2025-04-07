97aebe757fee8c6b4fa67216a73c01ba100a2072
Gus Ryan <120316725+GustuRyan@users.noreply.github.com>
2025-04-07 23:03:52 +0800

    update uncontained_layout_card.dart

--- lib/widgets/uncontained_layout_card.dart
+++ lib/widgets/uncontained_layout_card.dart
@@ -1,31 +1,31 @@
 import 'package:flutter/material.dart';
 
 class UncontainedLayoutCard extends StatelessWidget {
-  const UncontainedLayoutCard({super.key, required this.index, required this.label});
+  const UncontainedLayoutCard({
+    super.key,
+    required this.index,
+    required this.label,
+    required this.width,
+  });
 
   final int index;
   final String label;
+  final double width;
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: double.infinity,
-      margin: const EdgeInsets.symmetric(horizontal: 8.0), // Beri margin antar item
+      width: width,
+      margin: const EdgeInsets.only(right: 27.0),
       decoration: BoxDecoration(
-        color: Colors.primaries[index % Colors.primaries.length].withOpacity(0.5),
-        borderRadius: BorderRadius.circular(12), // Tambahkan border radius
-      ),
-      child: Center(
-        child: Text(
-          label,
-          style: const TextStyle(
-            color: Colors.white,
-            fontSize: 20,
-            fontWeight: FontWeight.bold,
-          ),
-          overflow: TextOverflow.ellipsis,
-          softWrap: false,
+        image: DecorationImage(
+          image: AssetImage(label),
+          fit: BoxFit.cover,
         ),
+        color: Colors.primaries[index % Colors.primaries.length].withOpacity(
+          0.5,
+        ),
+        borderRadius: BorderRadius.circular(12),
       ),
     );
   }
