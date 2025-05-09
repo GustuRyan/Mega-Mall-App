c67316caf80690cf7cc4b3d090099363ad6bbf81
Gus Ryan <120316725+GustuRyan@users.noreply.github.com>
2025-05-09 21:38:29 +0800

    create form_profile_work_assign_update.dart

---
+++ lib/presentation/module/profile_data/work_assign/widget/form_profile_work_assign_update.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:hr_360/core/style/style.dart';
+import 'package:hr_360/presentation/module/profile_data/work_assign/controller/profile_work_assign_update_controller.dart';
+
+class FormProfileWorkAssignUpdateWidget
+    extends GetResponsiveView<ProfileWorkAssignUpdateController> {
+  FormProfileWorkAssignUpdateWidget({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SizedBox(height: 5),
+        Expanded(
+          child: SingleChildScrollView(
+            scrollDirection: Axis.vertical,
+            child: Container(
+              margin: EdgeInsets.symmetric(horizontal: marginXs),
+              child: Container(),
+            ),
+          ),
+        ),
+        Container(
+          margin: EdgeInsets.all(marginMd),
+          width: Get.width,
+          child: Row(
+            children: [
+              Expanded(
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    padding: EdgeInsets.all(marginXs),
+                    backgroundColor: colorWhite,
+                    elevation: 0.5,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: borderRadiusXs,
+                      side: BorderSide(color: colorError, width: 0.4),
+                    ),
+                  ),
+                  child: Text(
+                    'Cancel',
+                    style: Get.textTheme.bodyMedium!.merge(
+                      TextStyle(color: colorError, fontSize: 12),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(width: marginMd),
+              Expanded(
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    padding: EdgeInsets.all(marginXs),
+                    backgroundColor: colorButton.withValues(alpha: 0.75),
+                    elevation: 0.5,
+                    shape: RoundedRectangleBorder(borderRadius: borderRadiusXs),
+                  ),
+                  child: Text(
+                    'Save',
+                    style: Get.textTheme.titleMedium!.merge(
+                      TextStyle(color: colorWhite, fontSize: 12),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
