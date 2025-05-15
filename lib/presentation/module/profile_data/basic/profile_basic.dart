4010d5b0b6197526422192317f897c25d44bc87e
Gus Ryan <120316725+GustuRyan@users.noreply.github.com>
2025-05-15 22:36:55 +0800

    create profile_basic.dart

---
+++ lib/presentation/module/profile_data/basic/profile_basic.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:hr_360/core/style/style.dart';
+import 'package:hr_360/presentation/module/profile_data/basic/controller/profile_basic_controller.dart';
+import 'package:hr_360/presentation/module/profile_data/basic/widget/form_profile_basic.dart';
+
+class ProfileBasicScreen extends GetResponsiveView<ProfileBasicController> {
+  ProfileBasicScreen({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return PopScope(
+      canPop: true,
+      child: GestureDetector(
+        onTap: () => FocusScope.of(context).unfocus(),
+        child: Scaffold(
+          appBar: AppBar(
+            automaticallyImplyLeading: false,
+            surfaceTintColor: null,
+            scrolledUnderElevation: 0,
+            centerTitle: true,
+            leading: IconButton(
+              onPressed: () => Get.back(),
+              icon: const Icon(Icons.arrow_back),
+            ),
+            title: Text(
+              'Basic Information',
+              style: Get.textTheme.titleLarge!.merge(
+                const TextStyle(fontWeight: FontWeight.w800),
+              ),
+            ),
+            backgroundColor: colorPrimary,
+          ),
+          backgroundColor: colorPrimary,
+          resizeToAvoidBottomInset: false,
+          body: SafeArea(child: FormProfileBasicWidget()),
+        ),
+      ),
+    );
+  }
+}
